8016517a23940f32bd2b0f78b295822d686e0507
Evammm <805416@sylvaniastudents.org>
2025-05-23 12:02:25 -0400

    boommmmm

--- tableTest.js
+++ tableTest.js
@@ -1,6 +1,60 @@
+class  Cell {
+  constructor(row, col, board) {
+      this.row = row;
+      this.col = col;
+      this.bomb = false;
+      this.board = board;
+      this.revealed = false;
+      this.flagged = false;
+  }
+
+  getAdjCells() {
+      var adj = [];
+      var lastRow = board.length - 1;
+      var lastCol = board[0].length - 1;
+      if (this.row > 0 && this.col > 0) adj.push(board[this.row - 1][this.col - 1]);
+      if (this.row > 0) adj.push(board[this.row - 1][this.col]);
+      if (this.row > 0 && this.col < lastCol) adj.push(board[this.row - 1][this.col + 1]);
+      if (this.col < lastCol) adj.push(board[this.row][this.col + 1]);
+      if (this.row < lastRow && this.col < lastCol) adj.push(board[this.row + 1][this.col + 1]);
+      if (this.row < lastRow) adj.push(board[this.row + 1][this.col]);
+      if (this.row < lastRow && this.col > 0) adj.push(board[this.row + 1][this.col - 1]);
+      if (this.col > 0) adj.push(board[this.row][this.col - 1]);       
+      return adj;
+  }
+
+  calcAdjBombs() {
+      var adjCells = this.getAdjCells();
+      var adjBombs = adjCells.reduce(function(acc, cell) {
+          return acc + (cell.bomb ? 1 : 0);
+      }, 0);
+      this.adjBombs = adjBombs;
+  }
+
+  flag() {
+      if (!this.revealed) {
+          this.flagged = !this.flagged;
+          return this.flagged;
+      }
+  }
+
+  reveal() {
+      if (this.revealed && !hitBomb) return;
+      this.revealed = true;
+      if (this.bomb) return true;
+      if (this.adjBombs === 0) {
+          var adj = this.getAdjCells();
+          adj.forEach(function(cell){
+              if (!cell.revealed) cell.reveal();
+          });
+      }
+      return false;
+  }
+}
+
 let board = document.getElementById("table");
 let size = 10;
-let totalBombs = 25;
+let totalBombs = 20;
 let board1 = buildArrays();
 
 
@@ -8,8 +62,20 @@ let board1 = buildArrays();
 
 
 function whap() {
-board.innerHTML = `<tr>${'<td class="game-cell">cell</td>'.repeat(size)}</tr>`.repeat(size);
-document.getElementById("whar").innerText = "boom";
+  board.innerHTML = "";
+  for (let row = 0; row < size; row++) {
+      let tr = document.createElement("tr");
+      for (let col = 0; col < size; col++) {
+          let td = document.createElement("td");
+          td.className = "game-cell";
+          td.setAttribute("data-row", row);
+          td.setAttribute("data-col", col);
+          td.innerText = "cell";
+          tr.appendChild(td);
+      }
+      board.appendChild(tr);
+  }
+  document.getElementById("whar").innerText = "boom";
 }
 
 function buildCells(){
@@ -55,9 +121,14 @@ function buildArrays() {
   }
 
   function check() {
-    runCodeForAllCells(function(cell) {
-        if (cell.bomb = true) {
-            cell.innerText = "bomb";
-        }
-    });
+    for (var row = 0; row<board1.length; row++) {
+      for (var col = 0; col<board1[0].length; col++) {
+        var cell = board1[row][col];
+        if (!cell.bomb) {
+          var td = document.querySelector(`[data-row="${cell.row}"][data-col="${cell.col}"]`);
+        td.innerText = "not bomb";
+        };
+      }
+    } 
+    document.getElementById("woah").innerText = "boom";
   }
